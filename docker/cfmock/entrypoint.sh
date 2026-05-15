4ebfa0467ac49b45fbc2a482e65a9bef483ecb09
meowpow-png <marin.softic@proton.me>
2026-05-15 10:46:41 +0200

    Stop using cfmock response seed

--- docker/cfmock/entrypoint.sh
+++ docker/cfmock/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -eu
 
-# sets up seed CIDR values
-cp -n /seed/ips.json /usr/share/nginx/html/ips.json
-
-# set CIDR values from env vars only if they exist,
-# otherwise skip and use seed values
 cfmock set
 
 exec nginx -g 'daemon off;'
